f1c68ee0505515047d1292c676123623491f4597
Gilmar FL Resende <gilmaresende@gmail.com>
2023-09-05 17:51:50 -0300

    table filter

--- web/cader/src/app/components/prime/data-table/data-table.component.ts
+++ web/cader/src/app/components/prime/data-table/data-table.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Table } from 'primeng/table';
+import { SEntidade } from 'src/app/core/model/sentidade';
 
 @Component({
   selector: 'data-table',
@@ -8,11 +8,24 @@ import { Table } from 'primeng/table';
 })
 export class DataTableComponent implements OnInit {
   customers: Array<{}> = [];
-
+  selected: SEntidade | undefined;
   header: Array<{ description: string; percentage: number }> = [];
   atributos: Array<string> = [];
-  //{ id: number; nome: string; idade: number; cidade: string }
-  data: Array<{}> = [{ id: 1, nome: 'A', idade: 10, cidade: 'bh' }];
+  dataShow: Array<{}> = [];
+  data: Array<{}> = [
+    { id: 1, nome: 'Gilmar', idade: 10, cidade: 'Itauna' },
+    { id: 2, nome: 'Livia', idade: 10, cidade: 'Crucilandia' },
+    { id: 3, nome: 'Thamires', idade: 10, cidade: 'Bom despache' },
+    { id: 1, nome: 'Gilmar', idade: 10, cidade: 'Itauna' },
+    { id: 2, nome: 'Livia', idade: 10, cidade: 'Crucilandia' },
+    { id: 3, nome: 'Thamires', idade: 10, cidade: 'Bom despache' },
+    { id: 1, nome: 'Gilmar', idade: 10, cidade: 'Itauna' },
+    { id: 2, nome: 'Livia', idade: 10, cidade: 'Crucilandia' },
+    { id: 3, nome: 'Thamires', idade: 10, cidade: 'Bom despache' },
+    { id: 1, nome: 'Gilmar', idade: 10, cidade: 'Itauna' },
+    { id: 2, nome: 'Livia', idade: 10, cidade: 'Crucilandia' },
+    { id: 3, nome: 'Thamires', idade: 10, cidade: 'Bom despache' },
+  ];
 
   ngOnInit() {
     this.header = [
@@ -21,6 +34,23 @@ export class DataTableComponent implements OnInit {
       { description: 'Idade', percentage: 25 },
       { description: 'Origem', percentage: 25 },
     ];
+    this.dataShow = this.data;
     this.atributos = ['id', 'nome', 'idade', 'cidade'];
+  }
+
+  public search(value: string) {
+    this.dataShow = [];
+    let filterMain: Array<{}> = [];
+    for (let atr of this.atributos) {
+      let filter = this.data.filter((res: any) => {
+        const str: string = res[atr].toString();
+        return !str.toLocaleLowerCase().indexOf(value.toLocaleLowerCase());
+      });
+      Array.prototype.push.apply(filterMain, filter);
+    }
+    const listClear = filterMain.filter(
+      (item, i) => filterMain.indexOf(item) === i
+    );
+    this.dataShow = listClear;
   }
 }
