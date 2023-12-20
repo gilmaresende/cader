6ea4e80844f7d4b15ebcaa01f04719575ef17c31
Gilmar <gilmaresende@gmail.com>
2023-12-19 22:35:18 -0300

    carregar botão opção customizada

--- web/cader/src/app/auth/dev/bi/create-bi/components/biparameter-item/form-parameter-biview/defined-parameter-view/defined-parameter-view.component.ts
+++ web/cader/src/app/auth/dev/bi/create-bi/components/biparameter-item/form-parameter-biview/defined-parameter-view/defined-parameter-view.component.ts
@@ -16,11 +16,37 @@ export class DefinedParameterViewComponent {
     value: new FormControl('', Validators.required),
   });
 
+  item?: BIParameterDefined;
+
   remove(item: BIParameterDefined) {}
 
   toForm(item: BIParameterDefined) {
+    this.item = item;
     const form = this.form.controls;
     form.name.setValue(item.name);
     form.value.setValue(item.value);
+  }
+
+  toOb() {
+    const form = this.form.controls;
+    this.item!.name = form.name.value as string;
+    this.item!.value = form.value.value as string;
+  }
+
+  toNew() {
+    const item: BIParameterDefined = {
+      name: '',
+      value: '',
+    };
+    this.toForm(item);
+  }
+
+  save() {
+    this.toOb();
+    const isItem = this.list.find((f) => f == this.item);
+    if (!isItem && this.item) {
+      this.list.push(this.item);
+    }
+    this.toNew();
   }
 }
